07c22a70df8cf6a94998dace37e9c08ec67a4b5c
Jorge Moratilla <jorge@moratilla.com>
2020-05-27 18:29:59 +0200

    add outputs.tf for terraform

---
+++ code/terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "Grafana URL" {
+  value = "http://${aws_instance.monitor.public_dns}:3000/"
+}
+
+output "Etcd URL" {
+  value = "http://${aws_elb.etcd_elb.public_dns}:2379/"
+}
+
+output "Benchmark SSH" {
+  value = "ssh -i ~/.ssh/${var.aws_key_name} admin${aws_instance.benchmark.public_dns}"
+}
